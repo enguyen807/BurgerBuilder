d9acd678dd1c73de254e7d65263329a60cec7fa4
Miles Le <eric.nguyen@fleishman.com>
2021-04-19 06:21:31 -0500

    Add responsiveness to sidedrawer and navbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import Auxiliary from "../../hoc/Auxiliary";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 import classes from "./Layout.css";
 
 const layout = (props) => (
   <Auxiliary>
     <Toolbar />
+    <SideDrawer />
     <main className={classes.Content}>{props.children}</main>
   </Auxiliary>
 );
